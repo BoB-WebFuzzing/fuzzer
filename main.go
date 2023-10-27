497fbbe09aed60751a1ba71a1f89ecfe45562505
kakunge <kakunge3@naver.com>
2023-10-28 07:26:19 +0900

    Update main.go

--- main.go
+++ main.go
@@ -2,17 +2,60 @@ package main
 
 import (
 	"fmt"
+	"os"
+
+	"github.com/fatih/color"
 )
 
-var configData config
-var requestData request
-
 func main() {
-	(*config).parseJSON(&configData, "json/config.json")
-	printConfig()
+	if len(os.Args) != 3 {
+		usage()
+	}
+
+	if checkFile(os.Args[1]) {
+		(*config).parseJSON(&configData, "json/config.json")
+	}
+
+	if checkFile(os.Args[2]) {
+		(*request).parseJSON(&requestData, "json/request_data.json")
+	}
+
+	// printConfig()
 
 	fmt.Println("------------------------------------------------------------")
 
-	(*request).parseJSON(&requestData, "json/request_data.json")
-	printRequest()
+	// printRequest()
+
+	fmt.Println("------------------------------------------------------------")
+
+	Login()
+
+	// fmt.Println("------------------------------------------------------------")
+
+	// testLogin()
+
+	for i := 0; i < configData.Cores; i++ {
+		initDir(i)
+	}
+
+	// test
+	runAFL("fuzzing-0")
+}
+
+func usage() {
+	fmt.Println("Usage : fuzzer <path of config file> <path of request data file>")
+	fmt.Println("Example : fuzzer config.json request_data.json")
+
+	os.Exit(-1)
+}
+
+func checkFile(fileName string) bool {
+    _, err := os.Stat(fileName)
+
+    if os.IsNotExist(err) {
+        color.Yellow("%v File does not exist. Please check your path.\n", fileName)
+		panic(err)
+	}
+
+	return true
 }
