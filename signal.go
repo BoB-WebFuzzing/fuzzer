483bda7f7312dca8b969831b7db250c266116551
kakunge <kakunge3@naver.com>
2023-10-28 07:23:56 +0900

    Update signal.go

--- signal.go
+++ signal.go
@@ -10,16 +10,17 @@ import (
 
 func exitAFL(c *exec.Cmd) {
 	interrupt := make(chan os.Signal, 1)
+
 	signal.Notify(interrupt, os.Interrupt, syscall.SIGINT)
 
-	fmt.Println("Waiting for SIGINT (Ctrl+C)...")
 	<-interrupt
 
 	process := c.Process
 	err := process.Signal(syscall.SIGINT)
+
 	if err != nil {
 		fmt.Println("Failed to send SIGINT:", err)
 	}
 
-	fmt.Println("SIGINT received. Exiting...")
+	fmt.Println("\nSIGINT received. Exiting...")
 }
