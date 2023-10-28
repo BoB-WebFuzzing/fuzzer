7f4deeb91e37a7a5c2a5ad1e770a2d87bf07d632
kakunge <kakunge3@naver.com>
2023-10-28 20:59:53 +0900

    Update afl.go - create fuzz stat

--- afl.go
+++ afl.go
@@ -1,18 +1,35 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/url"
 	"os"
 	"os/exec"
 	"strings"
+	"time"
 )
+
+type fuzzCampaignStatus struct {
+	TrialStart		string			`json:"trial_start"`
+	TrialComplete	bool			`json:"trial_complete"`
+	Targets			[]fuzzTarget	`json:"targets"`
+}
+
+type fuzzTarget struct {
+	TargetPath			string			`json:"target_path"`
+	Requests			[]string		`json:"requests"`
+	Methods				map[string]int	`json:"methods"`
+	IsSoapAction		bool			`json:"is_soapaction"`
+	LastCompletedTrial	int				`json:"last_completed_trial"`
+	LastCompletedRefuzz	int				`json:"last_completed_refuzz"`
+}
 
 func runAFL(fuzzingPath string) {
 	cmd := exec.Command("sh", fuzzingPath + "/run.sh")
-	
+
 	go exitAFL(cmd)
-	
+
 	output, _ := cmd.CombinedOutput()
 
 	os.WriteFile(fuzzingPath + "/output/fuzzer.log", output, 0644)
@@ -29,6 +46,7 @@ func initDir(i int) {
 
 	createScript(fuzzingDir)
 	createDict(fuzzingDir)
+	createFuzzStat(fuzzingDir)
 }
 
 func mkdir(dirName string) {
@@ -71,11 +89,13 @@ func createScript(fuzzingPath string) {
 	scriptContent += targets[0]
 
 	_, err = file.WriteString(scriptContent)
+
 	if err != nil {
 		panic(err)
 	}
 
 	err = os.Chmod(scriptPath, 0755)
+
 	if err != nil {
 		panic(err)
 	}
@@ -97,6 +117,62 @@ func createDict(fuzzingPath string) {
 	}
 
 	_, err = file.WriteString(dictContent)
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func createFuzzStat(fuzzingPath string) {
+	var fuzzStat fuzzCampaignStatus
+	uniqCheck := make(map[string]int)
+	targetIndex := 0
+	
+	fuzzStat.TrialStart = time.Now().Format("2006_01_02_15_04")
+	fuzzStat.TrialComplete = false
+	fuzzStat.Targets = []fuzzTarget{}
+
+	for key, value := range requestData.RequestsFound {
+		targetURL := strings.Split(value.URL, "?")[0]
+		method := strings.Split(key, " ")[0]
+		_, exist := uniqCheck[targetURL]
+
+		if exist {
+			fuzzStat.Targets[uniqCheck[targetURL]].Requests = append(fuzzStat.Targets[uniqCheck[targetURL]].Requests, key)
+			fuzzStat.Targets[uniqCheck[targetURL]].Methods[method]++
+		} else {
+			uniqCheck[targetURL] = targetIndex
+			targetIndex++
+
+			tempFuzzTarget := fuzzTarget{
+				Methods: make(map[string]int),
+			}
+
+			tempFuzzTarget.TargetPath = strings.Split(value.URL, "?")[0]
+			tempFuzzTarget.Requests = append(tempFuzzTarget.Requests, key)
+			tempFuzzTarget.Methods[method] = 1
+
+			fuzzStat.Targets = append(fuzzStat.Targets, tempFuzzTarget)
+		}
+	}
+
+	data, err := json.MarshalIndent(fuzzStat, "", "	")
+	data = []byte(strings.ReplaceAll(string(data), "\\u0026", "&"))
+
+	if err != nil {
+		panic(err)
+	}
+
+	file, err := os.Create(fuzzingPath + "/output/fuzz_stat.json")
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer file.Close()
+	
+	_, err = file.Write(data)
+
 	if err != nil {
 		panic(err)
 	}
